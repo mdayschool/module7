db8bf66423184ac9b816dd09e398d22b08bf4232
Michael Day <day.michael.b@gmail.com>
2019-07-05 19:50:40 -0500

    comments

--- fun_with_collections/search_sort_list.py
+++ fun_with_collections/search_sort_list.py
@@ -1,5 +1,6 @@
 
 def get_list():
+    """Gets and validates 3 inputs"""
     inputs = []
     for i in range(0,3):
         inp = float(input("Input a number between 1 and 50: "))
@@ -10,12 +11,17 @@ def get_list():
     return inputs
 
 def search_list(target):
+    """Searches list and returns -1 for value error"""
     try:
         return get_list().index(target)
     except ValueError:
         return -1
 
 def sort_list():
+    """
+    Sorts list - Returns a list because otherwise
+    it would just be generating garbage
+    """
     ls = get_list()
     ls.sort()
     return ls
