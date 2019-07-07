2e441397a5b729fac625570111a4aad138dea265
Michael Day <day.michael.b@gmail.com>
2019-07-07 10:59:52 -0500

    array sort red

---
+++ fun_with_collections/search_sort_array.py
@@ -0,0 +1,31 @@
+import array
+
+def get_list():
+    """Gets and validates 3 inputs"""
+    inputs = []
+    for i in range(0,3):
+        inp = float(input("Input a number between 1 and 50: "))
+        if inp >= 1 and inp <= 50:
+            inputs.append(inp)
+        else:
+            raise ValueError("Input must be between 1 and 50")
+    return inputs
+
+def search_list(target):
+    """Searches list and returns -1 for value error"""
+    try:
+        return get_list().index(target)
+    except ValueError:
+        return -1
+
+def sort_list():
+    """
+    Sorts list - Returns a list because otherwise
+    it would just be generating garbage
+    """
+    ls = get_list()
+    ls.sort()
+    return ls
+
+def sort_array():
+    pass
---
+++ test/test_search_sort_array.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import patch
+from fun_with_collections import search_sort_list
+from fun_with_collections import search_sort_array
+import array
+
+class BasicListTestCase(unittest.TestCase):
+    def test_get_list_below_range(self):
+        with patch('builtins.input', side_effect=[-1, 10, 45]):
+            with self.assertRaises(ValueError):
+                search_sort_list.get_list()
+    def test_get_list_above_range(self):
+        with patch('builtins.input', side_effect=[1, 10, 55]):
+            with self.assertRaises(ValueError):
+                search_sort_list.get_list()
+
+class SearchListTestCase(unittest.TestCase):
+    def test_search_list_present(self):
+        with patch('builtins.input', side_effect=[12, 25, 42]):
+            self.assertEqual(1, search_sort_list.search_list(25))
+    def test_search_list_absent(self):
+        with patch('builtins.input', side_effect=[12, 25, 42]):
+            self.assertEqual(-1, search_sort_list.search_list(34))
+
+class SortListTestCase(unittest.TestCase):
+    def test_sort_list(self):
+        with patch('builtins.input', side_effect=[42, 25, 12]):
+            self.assertEqual([12, 25, 42], search_sort_list.sort_list())
+
+class SortArrayTestCase(unittest.TestCase):
+    def test_sort_array(self):
+        test_array = array.array('i', [12, 25, 42])
+        with patch('builtins.input', side_effect=[42, 25, 12]):
+            self.assertEqual(test_array, search_sort_array.sort_array())
+
